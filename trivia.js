15e3908aaf4d05c842afa7f039bfac3d6a0fc3c2
bjouxzfish <bjouxzfish@gmail.com>
2023-07-16 00:57:49 -0700

    Fix issues with radio categoryby switching to button

--- trivia.js
+++ trivia.js
@@ -1,21 +1,26 @@
+// Constants for all sizes.
+const canvasWidth = 800;
+const canvasHeight = 500;
+
+const categoryDivWidth = 180;
+const headerY = 10;
+const qaButtonY = 100;
+const questionY = 150;
+const answerY = 350;
+
+// Variables for category.
+// This is the default category when the game starts.
+let current_category = 'geography';
+let category_buttons;
+const categories = ['geography', 'history', 'mathematics', 'people_and_places', 'religion_and_mythology', 'science_and_nature'];
+let questions_by_category;
+
 // Initialize variables for questions and answers.
 let questions;
 let question_text = "Click Next Question";
 let answer_text = "";
 let current_answer;
 
-// Variables for category.
-let radio_category;
-const categories = ['geography', 'history', 'mathematics', 'people_and_places', 'religion_and_mythology', 'science_and_nature'];
-let questions_by_category;
-
-// Constants for all sizes.
-const canvasWidth = 800;
-const canvasHeight = 500;
-const radioX = 160;
-const radioY = 70;
-const radioWidth = canvasWidth - 2 * radioX;
-const buttonY = 150;
 
 
 function preload() {
@@ -26,6 +31,8 @@ function preload() {
     url = base_url + category + '.json';
     questions_by_category.set(category, loadJSON(url));
   }
+  // Set the current questions to the default category.
+  questions = questions_by_category.get(current_category);
 
 }
 
@@ -40,65 +47,79 @@ function setup() {
   let buttonFontSize = '18px';
 
   button_reset = createButton('Next Question');
-  button_reset.position(200, buttonY);
+  button_reset.position(categoryDivWidth + 200, qaButtonY);
   button_reset.mousePressed(randomNewQuestion);
   button_reset.style('font-size', buttonFontSize);
 
   button_ans = createButton('Answer');
-  button_ans.position(400, buttonY);
+  button_ans.position(categoryDivWidth + 400, qaButtonY);
   button_ans.mousePressed(makeAnswerVisible);
   button_ans.style('font-size', buttonFontSize);
 
-  drawRadioDiv();
+  // Place a div for category selection.
+  drawCategoryDiv();
 
-  // Category selection using a radio.
-  radio_category = createRadio();
+  // Create a button for each category.
+  category_buttons = new Map();
   for (let i = 1; i <= categories.length; i++) {
-    radio_category.option(i, categories[i - 1]);
+    category = categories[i - 1]
+    category_buttons.set(category,
+      createButton(category)
+        .position(10, 30 * i)
+        .style('background-color', "#FFFFFF")
+        .mousePressed(createCategoryButtonMousePressedFunc(category)));
   }
-  // Set init value
-  radio_category.value("1");
-  // Set the width
-  radio_category.style("width", radioWidth.toString() + "px");
-  // Set the font size of the radio button
-  radio_category.style("font-size", "20px");
+  // Color the default category button during game initial setup.
+  colorSelectedCategoryButton(current_category);
 
-  // Position the radio-button object
-  radio_category.position(radioX, radioY);
-
-  setCategory();
 }
 
 
 function draw() {
   background('#222222');
   displayHeader();
-  drawRadioDiv();
-  setCategory();
+  drawCategoryDiv();
   displayQuestion();
   displayAnswer();
 }
 
-function drawRadioDiv() {
-  // Draw bottom board
+// SETTING CATEGORIES
+
+function drawCategoryDiv() {
+
   fill("#FAFAFA");
-  rect(radioX, radioY, radioWidth, 50);
+  rect(0, 0, categoryDivWidth, canvasHeight);
+  fill("#000000");
+  textSize(20);
+  textAlign(LEFT);
+  text("Category", 10, 20);
 }
 
-function setCategory() {
-  let radio_val;
-
-  if (radio_category.value().length === 0) {
-    // If no category selected, use the first category.
-    radio_val = 0;
-  } else {
-    radio_val = int(radio_category.value()) - 1;
+function createCategoryButtonMousePressedFunc(category_name) {
+  return function () {
+    setCurrentCategory(category_name);
+    colorSelectedCategoryButton(category_name);
   }
+}
 
-  let current_category = categories[radio_val];
+function setCurrentCategory(value) {
+  // console.log(value)
+  current_category = value;
   questions = questions_by_category.get(current_category);
 }
 
+function colorSelectedCategoryButton(value) {
+  for (let [key, button] of category_buttons) {
+    if (key == value) {
+      button.style('background-color', "#FFFF00");
+    } else {
+      button.style('background-color', "#FFFFFF")
+    };
+  };
+}
+
+
+// SETTING Q&A
 function randomNewQuestion() {
   var q_length = Object.keys(questions).length;
   var current_question = questions[
@@ -110,26 +131,25 @@ function randomNewQuestion() {
 
 function displayHeader() {
   textSize(40);
+  textAlign(CENTER);
   fill("#D3D3D3");
-  text("TRIVIA", 20, 10, 700, 300);
-}
-
-function displayCategories() {
-  textSize(30);
-  // Display the question.
-  text(question_text, 20, 150, 700, 300);
+  text("TRIVIA", categoryDivWidth, headerY, 700, 300);
 }
 
 function displayQuestion() {
   textSize(30);
+  textAlign(CENTER);
+  fill("#D3D3D3");
   // Display the question.
-  text(question_text, 20, 200, 700, 300);
+  text(question_text, categoryDivWidth, questionY, canvasWidth - categoryDivWidth, 300);
 }
 
 function displayAnswer() {
   textSize(30);
+  textAlign(CENTER);
+  fill("#D3D3D3");
   // Display the answer.
-  text(answer_text, 20, 350, 700, 500);
+  text(answer_text, categoryDivWidth, answerY, canvasWidth - categoryDivWidth, 500);
 }
 
 function makeAnswerVisible() {
